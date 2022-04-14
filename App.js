1e44f63d45e006993653ce8db9c0a1c9625a3b1d
adnan909 <m_adnan_93@yahoo.com>
2022-04-15 01:34:04 +0500

    timer camera expo

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type] = useState(Camera.Constants.Type.back);
+  const [uniqueId, setUniqueId] = useState(Math.random())
+  const [photo, setPhoto] = useState(null)
+
+  const camera = useRef();
+  const [isPlaying, setIsPlaying] = useState(false)
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  const onCameraReady = () => {
+    setIsPlaying(true)
+  }
+  const snap = async () => {
+    if (camera.current) {
+      let photo = await camera.current.takePictureAsync();
+      setPhoto(photo)
+      setIsPlaying(false)
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {photo ? <Image source={photo} style={{ width: 300, height: 300 }} /> :
+        <Camera ref={camera} style={styles.camera} type={type} onCameraReady={onCameraReady}>
+          <CountdownCircleTimer
+            key={uniqueId}
+            onComplete={snap}
+            isPlaying={isPlaying}
+            duration={3}
+            colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+            colorsTime={[7, 5, 2, 0]}
+          >
+            {({ remainingTime }) => <Text style={{ color: 'red' }}>{remainingTime}</Text>}
+          </CountdownCircleTimer>
+        </Camera>}
+      <TouchableOpacity onPress={() => {
+        setUniqueId(Math.random())
+        setIsPlaying(true)
+        setPhoto(null)
+      }}>
+        <Text>Restart Timer</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +63,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  camera: {
+    width: 300,
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
